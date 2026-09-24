83851b3c9371d40bdac20995752065ab73ea2a40
Aria Williams <awill9579@gmail.com>
2026-09-24 15:48:23 -0500

    refactor

--- Framework/CustomWinLoseAnims/custom_animation_screen.gd
+++ Framework/CustomWinLoseAnims/custom_animation_screen.gd
@@ -1,6 +1,8 @@
 class_name CustomAnimationScreen extends Control
 
 @export var sprite_frame_animator: AnimationPlayer
+@export var do_zoom_anims : bool = true
+@export var control_tween_sequencer: ControlTweenSequencer
 
 @warning_ignore("unused_signal")
 signal anim_finished
@@ -15,6 +17,10 @@ func play_anim() -> void:
 		push_warning("%s: sprite_frame_animator export var is null")
 		anim_finished.emit()
 		return
+	
+	if do_zoom_anims:
+		control_tween_sequencer.do_tween_sequence()
+	
 	sprite_frame_animator.play("default")
 	await get_tree().create_timer(2.0).timeout
 	anim_finished.emit()
